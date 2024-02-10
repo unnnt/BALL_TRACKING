e0448098b3728b52ae9c735a1931fad7898ca610
unnnt <d400200062@student.ums.ac.id>
2024-02-11 00:01:05 +0700

    tambah program cari yaw gyro, PID, driver motor. tambah inisialisasi pin driver

--- script_ardu/script_ardu.ino
+++ script_ardu/script_ardu.ino
@@ -1,28 +1,21 @@
-#include <Servo.h>
-
-Servo myServo;
-
-double setpoint = 320.0;  // Nilai tengah koordinat kamera
-double input, output;
-double Kp = 1.0;  // Konstanta Proporsional
-double Ki = 0.1;  // Konstanta Integral
-double Kd = 0.01; // Konstanta Derivatif
-
-unsigned long lastTime = 0;
-double elapsedTime, error, lastError;
-double cumError, rateError;
+#include "CFG.h"
+#include "GYRO.h"
+#include "MOTOR.h"
+#include "PID.h"
 
 void setup() {
-  Serial.begin(115200);
-  myServo.attach(9);  // Hubungkan servo ke pin 9
+  DEBUG_BEGIN(115200);
+  setup_pinmotor();
+  setup_mpu();
+  SERVO_PIN(9);
 }
 
 void loop() {
 //  if (Serial.available() > 0) {
-    int nilai_analog = Serial.parseInt();  // Baca nilai analog dari serial
+    int nilai_analog = random(0,100);  // Baca nilai analog dari serial
     nilai_analog = constrain(nilai_analog, 0, 640);
-
+    DEBUG_PRINTLN(nilai_analog);
     int pos_servo = map(nilai_analog, 0, 640, 0, 180);
-    myServo.write(pos_servo);  // Kontrol servo berdasarkan nilai analog
+    SERVO_WRITE(pos_servo);  // Kontrol servo berdasarkan nilai analog
 //  }
 }
