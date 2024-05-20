8abc885793ee9d3ad7c19796238680960182a68b
unnnt <d400200062@student.ums.ac.id>
2024-05-21 03:58:24 +0700

    tambah algoritma deteksi bola bertumpuk

--- baru_2/DETECT.h
+++ baru_2/DETECT.h
@@ -1,13 +1,22 @@
 void detect() {
   if (Serial.available() > 0) {
     String dataReceived = Serial.readStringUntil('\n');
-    dataReceived.trim();  // Menghapus spasi yang tidak perlu di awal dan akhir string
+    dataReceived.trim(); 
+    int spaceIndex = 0;
+    int prevSpaceIndex = 0;
 
-    // Memisahkan data menjadi bagian-bagian yang sesuai
-    x = dataReceived.substring(0, dataReceived.indexOf(' ')).toInt();
-    y = dataReceived.substring(dataReceived.indexOf(' ') + 1, dataReceived.indexOf(' ', dataReceived.indexOf(' ') + 1)).toInt();    
-    sisi = dataReceived.substring(dataReceived.indexOf(' ', dataReceived.indexOf(' ') + 1) + 1, dataReceived.lastIndexOf(' ')).toInt();  
-    persentase = dataReceived.substring(dataReceived.lastIndexOf(' ', dataReceived.lastIndexOf(' ') - 1) + 1, dataReceived.lastIndexOf(' ')).toInt();
-    jarak = dataReceived.substring(dataReceived.lastIndexOf(' ') + 1).toInt();
+    x = dataReceived.substring(prevSpaceIndex, spaceIndex = dataReceived.indexOf(' ', prevSpaceIndex)).toInt();
+    prevSpaceIndex = spaceIndex + 1;
+
+    y = dataReceived.substring(prevSpaceIndex, spaceIndex = dataReceived.indexOf(' ', prevSpaceIndex)).toInt();
+    prevSpaceIndex = spaceIndex + 1;
+
+    sisi = dataReceived.substring(prevSpaceIndex, spaceIndex = dataReceived.indexOf(' ', prevSpaceIndex)).toInt();
+    prevSpaceIndex = spaceIndex + 1;
+
+    persentase = dataReceived.substring(prevSpaceIndex, spaceIndex = dataReceived.indexOf(' ', prevSpaceIndex)).toFloat();
+    prevSpaceIndex = spaceIndex + 1;
+
+    jarak = dataReceived.substring(prevSpaceIndex).toFloat();
   }
 }
